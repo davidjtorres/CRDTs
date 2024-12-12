23079c4670405d5b4a81c6575d5f5c2b602c1bc1
David Torres <davidj_torres@hotmail.com>
2024-12-12 09:14:33 -0500

    adding logger settings

--- FlomaDocs/settings.py
+++ FlomaDocs/settings.py
@@ -12,11 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
