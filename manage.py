2f0db7c362bcc395ed4e649f4dc5b3ed68b5a378
David Torres <davidj_torres@hotmail.com>
2024-12-13 09:10:13 -0500

    updating app name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FlomaDocs.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "floma_docs_api.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
