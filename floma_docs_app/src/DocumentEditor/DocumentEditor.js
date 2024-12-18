fcd4140240fe8d68996bac3a39b19840c14eec40
David Torres <davidj_torres@hotmail.com>
2024-12-18 14:53:25 -0500

    small cahnges

--- floma_docs_app/src/DocumentEditor/DocumentEditor.js
+++ floma_docs_app/src/DocumentEditor/DocumentEditor.js
@@ -1,148 +1,144 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { useParams } from 'react-router-dom';
-import axiosInstance from './../AxiosInstance';
-import * as Y from 'yjs';
-import { QuillBinding } from 'y-quill';
-import Editor from './../Editor/Editor';
-import { useSession } from './../ContextProviders/SessionContext';
+import React, { useEffect, useState, useRef } from "react";
+import { useParams } from "react-router-dom";
+import axiosInstance from "./../AxiosInstance";
+import * as Y from "yjs";
+import { QuillBinding } from "y-quill";
+import Editor from "./../Editor/Editor";
+import { useSession } from "./../ContextProviders/SessionContext";
 
-const ORIGIN_REMOTE = 'remote';
+const ORIGIN_REMOTE = "remote";
 
 const DocumentEditor = () => {
-    const { document_id } = useParams();
-    const [document, setDocument] = useState(null);
-    const [email, setEmail] = useState('');
-    const wsRef = useRef(null);
-    const documentRef = useRef(new Y.Doc());
-    const quillRef = useRef();
-    const { user } = useSession();
+	const { document_id } = useParams();
+	const [document, setDocument] = useState(null);
+	const [email, setEmail] = useState("");
+	const wsRef = useRef(null);
+	const documentRef = useRef(new Y.Doc());
+	const quillRef = useRef();
+	const { user } = useSession();
 
-    const connect_socket = () => {
-        if (wsRef.current) return;
-        const ws = new WebSocket(
-            `http://localhost:8000/ws/documents/${document_id}/?token=${localStorage.getItem(
-                'token'
-            )}`
-        );
-        ws.binaryType = 'arraybuffer';
-        wsRef.current = ws;
-        ws.onopen = () => {
-            console.log('socket connected');
-        };
-        ws.onclose = () => {
-            console.log('socket disconnected');
-        };
+	const handleInvite = async () => {
+		try {
+			await axiosInstance.post(`/documents/${document_id}/invite/`, {
+				email,
+			});
+			// Refetch the document to update the collaborators list
+			const response = await axiosInstance.get(`/documents/${document_id}`);
+			setDocument(response.data);
+			setEmail(""); // Clear the input field
+		} catch (error) {
+			alert("Failed to invite collaborator");
+		}
+	};
 
-        ws.onmessage = (event) => {
-            documentRef.current.transact(() => {
-                const update = new Uint8Array(event.data);
-                Y.applyUpdate(documentRef.current, update);
-            }, ORIGIN_REMOTE);
-        };
-    };
+	const bind_document = () => {
+		// Bind Yjs document to Quill editor
 
-    const handleInvite = async () => {
-        try {
-            await axiosInstance.post(`/documents/${document_id}/invite/`, {
-                email,
-            });
-            // Refetch the document to update the collaborators list
-            const response = await axiosInstance.get(
-                `/documents/${document_id}`
-            );
-            setDocument(response.data);
-            setEmail(''); // Clear the input field
-        } catch (error) {
-            alert('Failed to invite collaborator');
-        }
-    };
+		const handleUpdate = (update, origin) => {
+			// Only send updates that originated locally
+			if (origin !== ORIGIN_REMOTE) {
+				wsRef.current.send(update);
+			}
+		};
 
-    const bind_document = () => {
-        // Bind Yjs document to Quill editor
+		documentRef.current.on("update", handleUpdate);
 
-        const handleUpdate = (update, origin) => {
-            // Only send updates that originated locally
-            if (origin !== ORIGIN_REMOTE) {
-                wsRef.current.send(update);
-            }
-        };
+		let quill = quillRef.current;
 
-        documentRef.current.on('update', handleUpdate);
+		//Binding Quill to Yjs document
+		new QuillBinding(documentRef.current.getText("content"), quill);
+	};
 
-        let quill = quillRef.current;
+	useEffect(() => {
+		const connect_socket = () => {
+			if (wsRef.current) return;
+			const ws = new WebSocket(
+				`http://localhost:8000/ws/documents/${document_id}/?token=${localStorage.getItem(
+					"token"
+				)}`
+			);
+			ws.binaryType = "arraybuffer";
+			wsRef.current = ws;
+			ws.onopen = () => {
+				console.log("socket connected");
+			};
+			ws.onclose = () => {
+				console.log("socket disconnected");
+			};
 
-        //Binding Quill to Yjs document
-        new QuillBinding(documentRef.current.getText('content'), quill);
-    };
+			ws.onmessage = (event) => {
+				documentRef.current.transact(() => {
+					const update = new Uint8Array(event.data);
+					Y.applyUpdate(documentRef.current, update);
+				}, ORIGIN_REMOTE);
+			};
+		};
 
-    useEffect(() => {
-        connect_socket();
-        return () => {
-            wsRef.current.close();
-        };
-    }, []);
+		connect_socket();
+		return () => {
+			wsRef.current.close();
+		};
+	}, [document_id]);
 
-    useEffect(() => {
-        const fetchDocument = async () => {
-            try {
-                const response = await axiosInstance.get(
-                    `/documents/${document_id}`
-                );
-                setDocument(response.data);
-            } catch (error) {
-                console.error('Failed to fetch document', error);
-            }
-        };
+	useEffect(() => {
+		const fetchDocument = async () => {
+			try {
+				const response = await axiosInstance.get(`/documents/${document_id}`);
+				setDocument(response.data);
+			} catch (error) {
+				console.error("Failed to fetch document", error);
+			}
+		};
 
-        fetchDocument();
-    }, [document_id]);
+		fetchDocument();
+	}, [document_id]);
 
-    if (!document) {
-        return <div>Loading...</div>;
-    }
+	if (!document) {
+		return <div>Loading...</div>;
+	}
 
-    return (
-        <div>
-            <h2>{document.title}</h2>
-            <div>
-                <Editor
-                    ref={quillRef}
-                    readOnly={false}
-                    onReady={() => {
-                        bind_document();
-                    }}
-                />
-            </div>
-            <div>
-                <h3>Owner</h3>
-                <p>
-                    {document.owner.username} ({document.owner.email}){' '}
-                </p>
-            </div>
-            <div>
-                <h3>Collaborators</h3>
-                <ul>
-                    {document.collaborators.map((collaborator) => (
-                        <li key={collaborator.id}>
-                            {collaborator.username} ({collaborator.email})
-                        </li>
-                    ))}
-                </ul>
-                {document?.owner.id === user?.id && (
-                    <div>
-                        <h3>Invite Collaborator</h3>
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Collaborator email"
-                        />
-                        <button onClick={handleInvite}>Invite</button>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<h2>{document.title}</h2>
+			<div>
+				<Editor
+					ref={quillRef}
+					readOnly={false}
+					onReady={() => {
+						bind_document();
+					}}
+				/>
+			</div>
+			<div>
+				<h3>Owner</h3>
+				<p>
+					{document.owner.username} ({document.owner.email}){" "}
+				</p>
+			</div>
+			<div>
+				<h3>Collaborators</h3>
+				<ul>
+					{document.collaborators.map((collaborator) => (
+						<li key={collaborator.id}>
+							{collaborator.username} ({collaborator.email})
+						</li>
+					))}
+				</ul>
+				{document?.owner.id === user?.id && (
+					<div>
+						<h3>Invite Collaborator</h3>
+						<input
+							type="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							placeholder="Collaborator email"
+						/>
+						<button onClick={handleInvite}>Invite</button>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default DocumentEditor;
